d24d7a4ba3406f85af0ba061fad0b8a36a4b9ebf
grapek9 <zerex93@gmail.com>
2014-12-10 14:45:37 +0100

    Drobne zmiany

--- NthDigitPI/src/NthDigit/NthDigit.java
+++ NthDigitPI/src/NthDigit/NthDigit.java
@@ -1,10 +1,22 @@
 package NthDigit;
+import java.util.Scanner;
 
 /**
  * Created by Daniel on 2014-12-10.
  */
 public class NthDigit {
     public static void main(String args[]){
+
+        int Digit=0;
+        Scanner liczba = new Scanner(System.in);
+        Digit = liczba.nextInt();
+        System.out.println("lolz"+Digit);
         System.out.println("NthDigitPI");
+        printA(Digit);
+
+
+    }
+    public static void printA(int a){
+    System.out.println("le a="+a);
     }
 }
