58cdbb29fda86f1878fd8cd8a822859c183ae4e0
grapek9 <zerex93@gmail.com>
2014-12-10 13:50:54 +0100

    Pierwszy Commit

---
+++ NthDigitPI/src/NthDigit/NthDigit.java
@@ -0,0 +1,10 @@
+package NthDigit;
+
+/**
+ * Created by Daniel on 2014-12-10.
+ */
+public class NthDigit {
+    public static void main(String args[]){
+        System.out.println("NthDigitPI");
+    }
+}
